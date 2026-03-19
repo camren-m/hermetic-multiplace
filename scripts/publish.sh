7fa4f3a2ae5eb6897346c6fb67b8a0e03ff3b310
typechecked <camrenmumme@gmail.com>
2026-03-19 18:15:32 -0400

    fix(orchestra): Use absolute path for publishing artifacts
    This reverts commit 0e9770bec98a6c888ebccfbe7ca8eeecdf5381b2.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -14,4 +14,4 @@ else
     printf "\e[1;33m-s provided, skipping build and using already built artifacts\e[0m\n"
 fi
 
-rbxcloud experience publish --filename ../../dist/world.rbxl --place-id $PLACE_ID --universe-id $EXPERIENCE_ID --version-type published --api-key $OC_API_KEY
+rbxcloud experience publish --filename $ORCHESTRA_DIR/dist/world.rbxl --place-id $PLACE_ID --universe-id $EXPERIENCE_ID --version-type published --api-key $OC_API_KEY
