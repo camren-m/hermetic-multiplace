1fe85c0b37208d36e394abe18467c823af0f65b7
typechecked <camrenmumme@gmail.com>
2026-03-19 20:26:44 -0400

    fix(upload): Skip pushing release if it already exists

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -14,22 +14,25 @@ else
     printf "\e[1;33m-s provided, skipping build and using already built artifacts\e[0m\n"
 fi
 
-if [ "$NODE_ENV" == "prod" ]; then
-    gh release create "v$RELEASE_NUMBER" \
-        --draft \
-        --latest \
-        --generate-notes \
-        --title "($RELEASE_NUMBER) $RELEASE_NAME"
-elif [ "$NODE_ENV" == "staging" ]; then
-    gh release create "v$RELEASE_NUMBER" \
-        --draft \
-        --prerelease \
-        --generate-notes \
-        --title "($RELEASE_NUMBER) $RELEASE_NAME"
+if gh release view "v$RELEASE_NUMBER" >/dev/null 2>&1; then
+    printf "\e[1;33mRelease v$RELEASE_NUMBER already exists, not pushing release\e[0m\n"
 else
-    printf "\e[1;33mNot in a staging or prod environment, not pushing release\e[0m\n"
-    exit
+    if [ "$NODE_ENV" == "prod" ]; then
+        gh release create "v$RELEASE_NUMBER" \
+            --draft \
+            --latest \
+            --generate-notes \
+            --title "($RELEASE_NUMBER) $RELEASE_NAME"
+    elif [ "$NODE_ENV" == "staging" ]; then
+        gh release create "v$RELEASE_NUMBER" \
+            --draft \
+            --prerelease \
+            --generate-notes \
+            --title "($RELEASE_NUMBER) $RELEASE_NAME"
+    else
+        printf "\e[1;33mNot in a staging or prod environment, not pushing release\e[0m\n"
+        exit
+    fi
+    gh release upload v$RELEASE_NUMBER dist/*.rbxl
+    gh release edit v$RELEASE_NUMBER --draft=false
 fi
-
-gh release upload v$RELEASE_NUMBER dist/*.rbxl
-gh release edit v$RELEASE_NUMBER --draft=false
