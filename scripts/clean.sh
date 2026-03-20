faa984bb7ce1d02da9e56f18b4d497a3bed32774
typechecked <camrenmumme@gmail.com>
2026-03-20 09:34:52 -0400

    chore(orchestra): Remove dead code

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -15,7 +15,6 @@ if [ "$LIMITED_CLEAN" == "0" ]; then
     printf "\e[1;31mPerforming full clean...\e[0m\n"
     rm -rf $ORCHESTRA_DIR/dist
     rm -rf $ORCHESTRA_DIR/node_modules
-    rm -f $ORCHESTRA_DIR/.lastenv
 else
     printf "\e[1;33mPerforming limited clean...\e[0m\n"
 fi
